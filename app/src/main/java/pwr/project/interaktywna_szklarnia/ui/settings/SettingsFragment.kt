a7b53b0b29e13ec5d1b7f3ae93abb4992d9733cc
mklimek585 <mateusz.klimek585@gmail.com>
2023-11-28 16:09:31 +0100

    Improved settings UI, testing themes switch

--- app/src/main/java/pwr/project/interaktywna_szklarnia/ui/settings/SettingsFragment.kt
+++ app/src/main/java/pwr/project/interaktywna_szklarnia/ui/settings/SettingsFragment.kt
@@ -3,12 +3,16 @@ package pwr.project.interaktywna_szklarnia.ui.settings
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
+import android.widget.PopupWindow
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import pwr.project.interaktywna_szklarnia.R
 import pwr.project.interaktywna_szklarnia.databinding.FragmentSettingsBinding
 
 
@@ -32,9 +36,8 @@ class SettingsFragment : Fragment() {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        binding.buttonLogout.setOnClickListener { view ->
-            testFun(view)
-        }
+//        binding.buttonLanguage.setOnClickListener { view -> chooseLanguage(view) }
+//        binding.buttonLogout.setOnClickListener { view -> logoutFun(view) }
         return root
     }
 
@@ -43,14 +46,36 @@ class SettingsFragment : Fragment() {
         _binding = null
     }
 
-    fun testFun(view: View) {
+    fun logoutFun(view: View) {
         Toast.makeText(context, "Logout", Toast.LENGTH_SHORT).show()
-        Log.i(TAG, "Invisible button")
+        Log.i(TAG, "Logout function")
     }
 
-//    fun popUP(view: View) {
-//        PopupMenu popup = new PopupMenu(context, view)
-//    }
+    fun chooseLanguage(view: View) {
+        // Tworzenie nowego PopupMenu
+        val popup = PopupMenu(context, view)
+        // Inflating menu z pliku XML
+        popup.menuInflater.inflate(R.menu.settings_menu, popup.menu)
+
+        // Ustawienie OnClickListener dla elementów menu
+        popup.setOnMenuItemClickListener { menuItem: MenuItem ->
+            when (menuItem.itemId) {
+                R.id.item_lang_pl -> {
+                    Toast.makeText(context, "Wybrano Polski", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.item_lang_eng -> {
+                    Toast.makeText(context, "Wybrano English", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        // Wyświetlanie PopupMenu
+        popup.show()
+    }
+
     // TODO popout menu
     // TODO popout activity o braku internetu
 }
